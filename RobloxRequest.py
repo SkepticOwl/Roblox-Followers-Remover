67f80aed4cf7f6e3ef3c538ccd126670fbc764bd
Νίκος Γκαραγκούνης <103339424+Nick-Gkaragkounis@users.noreply.github.com>
2022-08-12 04:33:13 +0300

    First commit

---
+++ RobloxRequest.py
@@ -0,0 +1,48 @@
+from types import SimpleNamespace 
+from time import sleep 
+import requests, json, re  
+
+class User():
+    cookie = ""
+    csrf = ""
+user = User()
+
+def update_csrf():
+    user.csrf = requests.post("https://auth.roblox.com/v2/logout", headers={"cookie":user.cookie}).headers["x-csrf-token"]
+
+def set_cookie(cookie):
+    user.cookie = ".ROBLOSECURITY="+re.sub("(.*)_", "", cookie)
+
+def to_object(str):
+    return json.loads(str, object_hook=lambda d: SimpleNamespace(**d))
+
+def request(method, url, has_cookie=False, has_csrf=False, data={}, to_json=False, content_type="application/json"):
+    headers = {"content-type": content_type}
+    if has_cookie: headers["cookie"] = user.cookie
+    if has_csrf: headers["x-csrf-token"] = user.csrf
+    response = requests.request(method, "https://"+re.sub("(.*)://", "", url), data=data, headers=headers)
+    if response.status_code == 200:
+        result = response.text 
+        if to_json: result = to_object(result)
+        return result 
+    elif response.status_code == 401 and (response.reason == 0 or response.reason.find("Unauthorized") != -1):
+        if has_cookie:
+            print("Unauthorized request, please enter a valid cookie")
+            set_cookie(input("Cookie: ").upper())
+        else: has_cookie = True 
+        return request(method, url, has_cookie, has_csrf, data, to_json)
+    elif response.status_code == 403 and (response.reason == 0 or response.reason.find("Token Validation Failed") != -1):
+        if has_csrf:
+            print("Request validation failed, updating csrf")
+            update_csrf()
+        else:
+            has_csrf = True 
+        return request(method, url, has_cookie, has_csrf, data, to_json)
+    elif response.status_code == 429 or response.status_code == 500:
+        print("Getting rate limited, waiting for 60 seconds")
+        sleep(60) 
+        return request(method, url, has_cookie, has_csrf, data, to_json)
+    else:
+        print("Request failed:", response.status_code, response.reason) 
+        return False 
+    
---
+++ main.py
@@ -0,0 +1,53 @@
+import RobloxRequest 
+
+def sanitize_input(prompt):
+    return input(prompt).lower().replace(" ", "")
+
+def ask(prompt):
+    response = sanitize_input(prompt)
+    while response != "no" and response != "yes":
+        print("Invalid response!")
+        response = sanitize_input(prompt) 
+    return response == "yes"
+
+def set_block_status(id, status):
+    url = "accountsettings.roblox.com/v1/users/{}/{}"
+    method = status and "block" or "unblock"
+    return RobloxRequest.request("POST", url.format(id, method), True, True)
+
+def remove_follower(follower):
+    blocked = set_block_status(follower.id, True) 
+    set_block_status(follower.id, False) 
+    return blocked != False 
+
+#very basic function to detect bots, you can rewrite this and use a more efficient method
+def isBot(follower):
+    url_format = "friends.roblox.com/v1/users/{}/{}/count"
+    followings_num = RobloxRequest.request("GET", url_format.format(follower.id, "followings"), to_json=True).count
+    friends_num = RobloxRequest.request("GET", url_format.format(follower.id, "friends"), to_json=True).count 
+    return followings_num == 0 and friends_num == 0
+
+def main():
+    RobloxRequest.set_cookie(input("Cookie: ").upper())
+    user_info = RobloxRequest.request("GET", "users.roblox.com/v1/users/authenticated", True, to_json=True) 
+    print("Authenticated as", user_info.name)
+    if not ask("Continue? yes/no "): exit() 
+    only_bots = ask("Only unfollow bots? yes/no ")
+    url_format = "friends.roblox.com/v1/users/{}/followers?cursor={}"
+    cursor = ""
+    RobloxRequest.update_csrf()
+    while True:
+        result = RobloxRequest.request("GET", url_format.format(user_info.id, cursor), to_json=True)
+        cursor = result.nextPageCursor 
+        for follower in result.data:
+            if only_bots and isBot(follower) or not only_bots:
+                success = remove_follower(follower)
+                if success:
+                    print("Removed follower:", follower.name)
+                else:
+                    print("REMOVAL FAILED:", follower.name) 
+        if cursor == None: break 
+    print("Script execution finished")
+
+if __name__ == "__main__":
+    main()
